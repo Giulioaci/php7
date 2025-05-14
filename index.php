725f77a65a68984f9e34c22a2d345b95e29165a6
Giulioaci <giulioacitorio@yahoo.it>
2025-05-14 12:57:03 +0200

    php7
    esercizio terminato

---
+++ index.php
@@ -0,0 +1,75 @@
+<?php
+
+function checklength($string){
+    if(strlen($string)>=8){
+        return true;
+    }else{
+        echo "la password deve contenere almeno 8 caratteri \n";
+    return false;
+    }
+}
+
+
+
+
+function checkuppercase($string){
+    for($i=0;  $i<strlen($string); $i++){
+        if(ctype_upper($string[$i])){
+           return true ;
+        }else{
+            echo "manca una lettera maiuscola \n";
+            return false;
+        }
+    }
+}
+
+
+
+function checknumber($string){
+    for($i=0;  $i<strlen($string); $i++){
+        if(is_numeric($string[$i])){
+           return true ;
+        }
+        
+    }
+    echo "almeno un numero \n";
+            return false;
+}
+
+
+
+function checkspecial($string){
+    $chars = ['&','?', '#','!'];
+    for($i=0;  $i<strlen($string); $i++){
+        if(in_array($string[$i],$chars)){
+            return true;
+        }
+    }
+    echo "almeno un carattere speciale \n";
+    return false;
+}
+
+
+
+function checkpassword($string){
+    $check = checklength($string);
+    $check2 = checkuppercase($string);
+    $check3 = checknumber($string);
+    $check4 = checkspecial($string);
+
+    if($check && $check2 && $check3 && $check4){
+        echo "password accettata \n";
+    }
+     return $check && $check2 && $check3 && $check4;
+}
+
+
+do{
+    $password = readline("inserisci password: \n");
+}while(!checkpassword($password));
+
+
+
+
+
+
